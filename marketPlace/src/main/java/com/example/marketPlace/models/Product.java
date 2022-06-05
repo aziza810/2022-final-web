fe0fa45bb83935cc386ca72f9706c5541a570fc5
Aziza Dzhamangulova <81962842+aziza810@users.noreply.github.com>
2022-06-05 23:36:23 +0600

    add version 0.0.1

---
+++ marketPlace/src/main/java/com/example/marketPlace/models/Product.java
@@ -0,0 +1,46 @@
+package com.example.marketPlace.models;
+
+import com.example.marketPlace.services.ProductService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String price;
+    private String city;
+    private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
+
+
+}
