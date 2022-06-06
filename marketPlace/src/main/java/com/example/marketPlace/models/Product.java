e4b2dceb1e8257c180f54cc03c8c49081890ce57
Aziza Dzhamangulova <81962842+aziza810@users.noreply.github.com>
2022-06-06 21:18:17 +0600

    add last version

--- marketPlace/src/main/java/com/example/marketPlace/models/Product.java
+++ marketPlace/src/main/java/com/example/marketPlace/models/Product.java
@@ -1,12 +1,8 @@
 package com.example.marketPlace.models;
 
-import com.example.marketPlace.services.ProductService;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,33 +10,29 @@ import java.util.List;
 @Entity
 @Table(name = "products")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
-    private String price;
+    private Integer price;
     private String city;
-    private String author;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+            mappedBy = "product")
     private List<Image> images = new ArrayList<>();
-
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
     private Long previewImageId;
     private LocalDateTime dateOfCreated;
 
     @PrePersist
-    private void init(){
-        dateOfCreated = LocalDateTime.now();
-    }
+    private void onCreate() { dateOfCreated = LocalDateTime.now(); }
+
 
     public void addImageToProduct(Image image) {
         image.setProduct(this);
         images.add(image);
     }
-
-
 }
